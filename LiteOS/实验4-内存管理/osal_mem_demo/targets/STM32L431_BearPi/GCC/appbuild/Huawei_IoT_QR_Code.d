db35d940a4b0502d5e0d64db81660ed0f6e39098
HYYZRLY <1033375842@qq.com>
2021-07-09 21:04:00 +0800

    内存管理实验代码

---
+++ LiteOS/实验4-内存管理/osal_mem_demo/targets/STM32L431_BearPi/GCC/appbuild/Huawei_IoT_QR_Code.d
@@ -0,0 +1,2 @@
+appbuild/Huawei_IoT_QR_Code.d appbuild/Huawei_IoT_QR_Code.o: \
+ D:/BearPi/LiteOS/osal_mem_demo/targets/STM32L431_BearPi/Src/Huawei_IoT_QR_Code.c
---
+++ LiteOS/实验4-内存管理/osal_mem_demo/targets/STM32L431_BearPi/GCC/appbuild/shell_main.d
@@ -0,0 +1,14 @@
+appbuild/shell_main.d appbuild/shell_main.o: \
+ C:/Users/LY/.icode/sdk/IoT_LINK_1.0.0/iot_link/shell/shell_main.c \
+ C:/Users/LY/.icode/sdk/IoT_LINK_1.0.0/iot_link/inc/shell.h \
+ C:/Users/LY/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/osal/osal.h \
+ C:/Users/LY/.icode/sdk/IoT_LINK_1.0.0/iot_link/link_misc/link_misc.h \
+ C:/Users/LY/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/osal/osal_types.h
+
+C:/Users/LY/.icode/sdk/IoT_LINK_1.0.0/iot_link/inc/shell.h:
+
+C:/Users/LY/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/osal/osal.h:
+
+C:/Users/LY/.icode/sdk/IoT_LINK_1.0.0/iot_link/link_misc/link_misc.h:
+
+C:/Users/LY/.icode/sdk/IoT_LINK_1.0.0/iot_link/os/osal/osal_types.h:
